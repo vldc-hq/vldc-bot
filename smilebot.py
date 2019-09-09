2d686d68bedd52958ced45ec9b962424db9d9907
Sergey Melekhin <sergey@melekhin.me>
2019-09-09 17:53:51 +1000

    working quarantine logic

--- smilebot.py
+++ smilebot.py
@@ -7,12 +7,13 @@
 import logging
 import os
 import datetime
+import functools
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, BaseFilter
 from telegram.ext.dispatcher import run_async
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+                    level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
@@ -24,7 +25,9 @@ QUARANTINE = {}
 # list of users in quarantine joined in a particular minute
 QUARANTINE_MIN = [[] for x in range(60)]
 
-GROUP_CHAT_ID = '@vldchat'
+GROUP_CHAT_ID = '@dev_smile_test'
+BOT_NAME = 'smile_bot_dev'
+LOG_ALL_CALLS = True
 
 
 class _Admin(BaseFilter):
@@ -35,8 +38,21 @@ class _Admin(BaseFilter):
         return message.from_user.id in set([a.user.id for a in message.chat.get_administrators()])
 
 
+def log_call(f):
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        global LOG_ALL_CALLS
+        if LOG_ALL_CALLS:
+            logging.debug("calling {}".format(f.__name__))
+        return f(*args, **kwargs)
+    return inner
+
+
 @run_async
+@log_call
 def quarantine_user(user, chat_id, context):
+    global QUARANTINE
+    global QUARANTINE_MIN
     minute = datetime.datetime.utcnow().minute
     QUARANTINE[user.id] = minute
     QUARANTINE_MIN[minute].append(user.id)
@@ -66,34 +82,53 @@ def help_(update, context):
                               "'/off' – smile mode OFF\n")
 
 
+@log_call
 def kick(chat_id, user_id, bot):
     if not bot.kick_chat_member(chat_id, user_id):
         logger.log(logging.INFO, "failed to kick user {}".format(user_id))
 
 
+@log_call
 def kick_users(chat_id, bot):
+    global QUARANTINE
+    global QUARANTINE_MIN
     minute = (datetime.datetime.utcnow().minute + 1) % len(QUARANTINE_MIN)
     slowpokes = list([x for x in QUARANTINE_MIN[minute]])
     for user_id in slowpokes:
-        kick(chat_id, user_id, bot)
-        del(QUARANTINE, user_id)
-        QUARANTINE_MIN[minute].remove(user_id)
+        try:
+            kick(chat_id, user_id, bot)
+            del(QUARANTINE, user_id)
+            QUARANTINE_MIN[minute].remove(user_id)
+        except Exception as e:
+            logger.exception(e)
 
 
-def callback_minute(context: telegram.ext.CallbackContext):
+@log_call
+def callback_minute(context):
+    global GROUP_CHAT_ID
     try:
         kick_users(GROUP_CHAT_ID, context.bot)
     except Exception as e:
         logger.exception(e)
 
 
+@run_async
+@log_call
 def welcome(update, context):
+    global QUARANTINE
+    global QUARANTINE_MIN
     minute = QUARANTINE.get(update.effective_user.id)
     if minute == None:
         return
-    del(QUARANTINE, update.effective_user.id)
-    QUARANTINE_MIN[minute].remove(update.effective_user.id)
-    update.message.reply_text("Welcome to VLDC!")
+    if update.effective_message.reply_to_message != None and \
+            update.effective_message.reply_to_message.from_user.id == context.bot.get_me().id:
+        del(QUARANTINE[update.effective_user.id])
+        QUARANTINE_MIN[minute].remove(update.effective_user.id)
+        update.message.reply_text("Welcome to VLDC!")
+    else:
+        context.bot.delete_message(
+            update.effective_chat.id,
+            update.effective_message.message_id, 10)
 
 
 @run_async
@@ -124,6 +159,7 @@ def sml_mode_off(update, context):
 @run_async
 def smile(update, context):
     """ Delete all messages except stickers or GIFs """
+    global SMILE_MODE
     if SMILE_MODE:
         context.bot.delete_message(
             update.effective_chat.id,
@@ -136,12 +172,14 @@ def error(update, context):
 
 
 @run_async
+@log_call
 def handle_new_chat_members(update, context):
     msg = update.message
     for user in msg.new_chat_members:
         quarantine_user(user, update.effective_chat.id, context)
 
 
+@log_call
 def main():
     """ Start the Smile! 😊."""
     TOKEN = os.getenv("TOKEN", None)
@@ -159,20 +197,20 @@ def main():
     dp.add_handler(CommandHandler("on", sml_mode_on, filters=admin))
     dp.add_handler(CommandHandler("off", sml_mode_off, filters=admin))
 
+    # on user join start quarantine mode
+    dp.add_handler(MessageHandler(
+        Filters.status_update.new_chat_members,
+        handle_new_chat_members))
+
     # remove messages from users in quarantine
     dp.add_handler(MessageHandler(
-        Filters.group,
+        Filters.group & Filters.text,
         welcome))
 
     # on non sticker or gif message - delete the message
     dp.add_handler(MessageHandler(
         ~Filters.sticker & ~Filters.animation,
         smile))
-
-    # on user join start quarantine mode
-    dp.add_handler(MessageHandler(
-        Filters.status_update.new_chat_members,
-        handle_new_chat_members))
 
     dp.add_error_handler(error)
     j = updater.job_queue
