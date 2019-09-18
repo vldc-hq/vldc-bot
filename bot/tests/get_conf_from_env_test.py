5664470f0bfc64624adb2746906cd92e009dafef
Vadim Iskuchekov <root@egregors.com>
2019-09-18 20:14:14 +0200

    test

--- bot/tests/get_conf_from_env_test.py
+++ bot/tests/get_conf_from_env_test.py
@@ -10,13 +10,19 @@ class ConfigTestCase(TestCase):
         self.env_debug = "True"
         self.env_chat_id = "@vldc_best_chat"
         self.env_token = "my-secret-token"
+        self.env_mongo_initdb_root_username = "root"
+        self.env_mongo_initdb_root_password = "my-mega-secret-password"
 
         os.environ["DEBUG"] = self.env_debug
         os.environ["CHAT_ID"] = self.env_chat_id
         os.environ["TOKEN"] = self.env_token
+        os.environ["MONGO_INITDB_ROOT_USERNAME"] = self.env_mongo_initdb_root_username
+        os.environ["MONGO_INITDB_ROOT_PASSWORD"] = self.env_mongo_initdb_root_password
 
     def test_get_config(self):
         c: Dict = get_config()
         self.assertEqual(c["DEBUG"], self.env_debug)
         self.assertEqual(c["GROUP_CHAT_ID"], self.env_chat_id)
         self.assertEqual(c["TOKEN"], self.env_token)
+        self.assertEqual(c["MONGO_USER"], self.env_mongo_initdb_root_username)
+        self.assertEqual(c["MONGO_PASS"], self.env_mongo_initdb_root_password)
