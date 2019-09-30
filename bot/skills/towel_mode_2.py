9a35f438c106a017816d481560c609a5d0b48dee
Vadim Iskuchekov <root@egregors.com>
2019-09-30 23:34:32 +0200

    remove msg form quarantine users

--- bot/skills/towel_mode_2.py
+++ bot/skills/towel_mode_2.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
 
+from pymongo.collection import Collection
 from telegram import Update, User, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 
@@ -14,7 +15,7 @@ logger = logging.getLogger(__name__)
 # todo: extract maybe?
 class DB:
     def __init__(self, db_name: str):
-        self._coll = get_db(db_name).quarantine
+        self._coll: Collection = get_db(db_name).quarantine
 
     def add_user(self, user_id: str):
         self._coll.insert_one({
@@ -22,9 +23,24 @@ class DB:
             "datetime": datetime.now()
         })
 
+    def find_user(self, user_id: str):
+        return self._coll.find_one({"user_id": user_id})
+
 
 db = DB("towel_mode")
 mode = Mode(mode_name="towel_mode", default=True)
+
+
+def quarantine_filter(update: Update, context: CallbackContext):
+    user_id = update.effective_user.id
+    # todo: cache it
+    user = db.find_user(user_id)
+    # if user exist -> remove message
+    if user is not None:
+        context.bot.delete_message(
+            update.effective_chat.id,
+            update.effective_message.message_id, 10
+        )
 
 
 @mode.add
@@ -37,6 +53,11 @@ def add_towel_mode(upd: Updater, handlers_group: int):
                    handlers_group)
 
     # remove messages from users from quarantine
+    dp.add_handler(MessageHandler(
+        Filters.group & ~Filters.status_update, quarantine_filter),
+        handlers_group
+    )
+
     # catch "I am a bot" button press and instantly ban!
     # kick all not replied users (check every minutes)
 
@@ -49,6 +70,7 @@ def quarantine_user(user: User, chat_id: str, context: CallbackContext):
         [InlineKeyboardButton("Я не бот!", callback_data=MAGIC_NUMBER)]])
     context.bot.send_message(
         chat_id,
+        # todo: rewrite test
         f"{user.name} Нажми кнопку ниже, чтобы доказать, что ты не бот.\n"
         "Я буду удалять твои сообщения, пока ты не сделаешь это.\n"
         "А коли не сделаешь, через час выкину из чата.\n"
