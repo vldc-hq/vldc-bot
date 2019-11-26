1c9702d9fda77f2aeceb1e8e3cf86ddc0d561076
Andrei Sobolevskii <zemlya25@gmail.com>
2019-11-26 17:31:48 +1000

    Fix #55 - added 2k years modifyer

--- bot/skills/still.py
+++ bot/skills/still.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, run_async, CallbackContext
@@ -12,9 +13,18 @@ def add_still(upd: Updater, handlers_group: int):
     dp.add_handler(CommandHandler("still", still), handlers_group)
 
 
+def to_2k_year(year: int):
+    year_2k = str(year)
+    if (year // 100) % 10 == 0:
+        y = list(str(year))
+        y[-3] = 'k'
+        year_2k = ''.join(y)
+    return year_2k
+
+
 @run_async
 def still(update: Update, context: CallbackContext):
     text = " ".join(context.args)
     chat_id = update.effective_chat.id
     if text:
-        context.bot.send_message(chat_id, f"Вот бы сейчас {text} в 2k19 лул 😹😹😹")
+        context.bot.send_message(chat_id, f"Вот бы сейчас {text} в {to_2k_year(datetime.now().year)} лул 😹😹😹")
