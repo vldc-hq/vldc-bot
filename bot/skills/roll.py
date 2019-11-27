8e87c839e32da540d3f00ca08d175bccaa0a6cc5
Vadim Iskuchekov <root@egregors.com>
2019-11-27 11:59:45 +1000

    russian roulette fix, bump 0.8.6, resolved #61

--- bot/skills/roll.py
+++ bot/skills/roll.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime, timedelta
 from random import choice
+from typing import List
 
 from telegram import Update, User
 from telegram.ext import Updater, CommandHandler, CallbackContext, run_async
@@ -9,6 +10,7 @@ logger = logging.getLogger(__name__)
 
 # 24h
 MUTE_MINUTES = 1440
+BARREL_SIZE = 6
 
 
 def add_roll(upd: Updater, handlers_group: int):
@@ -17,18 +19,23 @@ def add_roll(upd: Updater, handlers_group: int):
     dp.add_handler(CommandHandler("roll", roll), handlers_group)
 
 
-def _shot():
-    empty, bullet = False, True
-    return choice([empty, empty, bullet, empty, empty, empty])
+def _get_barrel() -> List[bool]:
+    return [choice([True, False]) for _ in range(BARREL_SIZE)]
+
+
+def _shot() -> bool:
+    barrel = _get_barrel()
+    logger.info(f"the barrel is {barrel}")
+    return choice(barrel)
 
 
 @run_async
 def roll(update: Update, context: CallbackContext):
     user: User = update.effective_user
-    is_miss = _shot()
+    is_shoot = _shot()
     logger.info(f"user: {user.full_name}[{user.id}] is rolling and... "
-                f"{'miss!' if is_miss else 'he is dead!'}")
-    if is_miss:
+                f"{'miss!' if is_shoot else 'he is dead!'}")
+    if is_shoot:
         update.message.reply_text(f"🔫 MISS! 😎")
     else:
         until = datetime.now() + timedelta(minutes=MUTE_MINUTES)
