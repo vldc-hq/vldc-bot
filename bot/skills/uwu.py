94554551e19e2c392ebab9c7a3726ed39916d6c1
Зяблицкий Михаил Владимирович <19762@7733.ru>
2020-01-16 14:34:23 +1000

    rm uwu cmd, check every text msg by regexp-filter

--- bot/skills/uwu.py
+++ bot/skills/uwu.py
@@ -2,7 +2,9 @@ import logging
 import random
 
 from telegram import Update
-from telegram.ext import CommandHandler, Updater, CallbackContext, run_async
+from telegram.ext import MessageHandler, Updater, CallbackContext, run_async
+
+from filters import uwu_filter
 
 logger = logging.getLogger(__name__)
 
@@ -10,19 +12,17 @@ logger = logging.getLogger(__name__)
 def add_uwu(upd: Updater, handlers_group: int):
     logger.info("register uwu handlers")
     dp = upd.dispatcher
-    dp.add_handler(CommandHandler("uwu", uwu), handlers_group)
+    dp.add_handler(MessageHandler(uwu_filter, uwu), handlers_group)
 
 
 @run_async
 def uwu(update: Update, context: CallbackContext):
     well_prepared_anti_UwU_imgs = [
-        'AgADAgADG6wxG4hCMEtTOdyVipxZzgJ-XA8ABAEAAwIAA3gAA0xGAgABFgQ',
-        'AgADAgADbawxG29dMUuBUJdCgzngPjbQug8ABAEAAwIAA3gAA5sxBgABFgQ',
-        'AgADAgADHawxG4hCMEv7_rCj61FgpRWqwg8ABAEAAwIAA3gAA8czAQABFgQ',
-        'AgADAgADHqwxG4hCMEsTAlVxUnCKEH2Xwg8ABAEAAwIAA3gAAyg0AQABFgQ',
-        'AgADAgADH6wxG4hCMEszk4hv-RkUlkVuXA8ABAEAAwIAA3gAAzpDAgABFgQ'
+        'AgADAgADiqwxG2pVAAFJ2TIJLoDENOSxBcEOAAQBAAMCAANtAAO1lQEAARYE',
+        'AgADAgADiawxG2pVAAFJU823IoEQJvfOpcIPAAQBAAMCAANtAAOpKwMAARYE',
+        'AgADAgADiqwxG2pVAAFJ2TIJLoDENOSxBcEOAAQBAAMCAANtAAO1lQEAARYE'
     ]
-
     chat_id = update.effective_chat.id
+    message_id = update.message.message_id
     fairly_random_pic = random.choice(well_prepared_anti_UwU_imgs)
-    context.bot.send_photo(chat_id, photo=fairly_random_pic, caption="don't uwu! 😡")
+    context.bot.send_photo(chat_id, reply_to_message_id=message_id, photo=fairly_random_pic, caption="don't uwu! 😡")
