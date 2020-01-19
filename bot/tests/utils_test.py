d9ea75def1ce8d59952d59b2ad19377b70e11ee2
egregors <root@egregors.com>
2020-01-19 16:29:26 +1000

    get duration func, tests

---
+++ bot/tests/utils_test.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+from unittest import TestCase
+
+from utils.time import get_duration
+
+
+class DurationParserTestCase(TestCase):
+    def test_get_secs(self):
+        self.assertEqual(get_duration('20s'), timedelta(seconds=20))
+        self.assertEqual(get_duration('20s  '), timedelta(seconds=20))
+        self.assertEqual(get_duration('     20s  '), timedelta(seconds=20))
+
+    def test_get_mins(self):
+        self.assertEqual(get_duration('20m'), timedelta(minutes=20))
+        self.assertEqual(get_duration('20m  '), timedelta(minutes=20))
+        self.assertEqual(get_duration('     20m  '), timedelta(minutes=20))
+
+    def test_get_hours(self):
+        self.assertEqual(get_duration('20h'), timedelta(hours=20))
+        self.assertEqual(get_duration('20h  '), timedelta(hours=20))
+        self.assertEqual(get_duration('     20h  '), timedelta(hours=20))
+
+    def test_get_mix(self):
+        self.assertEqual(get_duration('2h 30m 10s'), timedelta(hours=2, minutes=30, seconds=10))
+        self.assertEqual(get_duration('2h   30m 10s'), timedelta(hours=2, minutes=30, seconds=10))
+        self.assertEqual(get_duration('     2h   30m 10s'), timedelta(hours=2, minutes=30, seconds=10))
+
+    def test_get_default(self):
+        self.assertEqual(get_duration('600'), timedelta(minutes=600))
+        self.assertEqual(get_duration('600  '), timedelta(minutes=600))
+        self.assertEqual(get_duration('     600  '), timedelta(minutes=600))
+
+    def test_get_bad_raw_duration(self):
+        self.assertEqual(get_duration('php sucks!'), timedelta(0))
+        self.assertEqual(get_duration('php 20s sucks!'), timedelta(seconds=20))
+        self.assertEqual(get_duration('php 20s sucks 40m !'), timedelta(seconds=20, minutes=40))
