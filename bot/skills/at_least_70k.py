1e41a5434fb83f00d6a7bc5ead332dbffb5eb3e6
egregors <root@egregors.com>
2020-04-01 13:35:04 +1000

    at least 70k, bump 0.22

---
+++ bot/skills/at_least_70k.py
@@ -0,0 +1,23 @@
+import logging
+
+from telegram import Update, User
+from telegram.ext import Updater, Dispatcher, CommandHandler, CallbackContext, run_async
+
+logger = logging.getLogger(__name__)
+
+MSG = f"Значительно больше откликов на предложение можно получить, " \
+      f"если подробно изложить суть, приложив по возможности ссылку на описание и " \
+      f"указав вилку :3"
+
+
+def add_70k(upd: Updater, handlers_group: int):
+    logger.info("registering 70k handler")
+    dp: Dispatcher = upd.dispatcher
+    dp.add_handler(CommandHandler("70k", _70k), handlers_group)
+
+
+@run_async
+def _70k(update: Update, context: CallbackContext):
+    user: User = update.message.reply_to_message.from_user if update.message.reply_to_message else None
+    msg = f"@{user.username} " + MSG if user else MSG
+    context.bot.send_message(update.effective_chat.id, msg)
