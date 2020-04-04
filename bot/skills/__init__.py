16e6ea011114a5666572d9dc553821864aa48dec
egregors <root@egregors.com>
2020-04-04 14:43:58 +1000

    bump 0.23

--- bot/skills/__init__.py
+++ bot/skills/__init__.py
@@ -21,7 +21,7 @@ from skills.towel_mode import add_towel_mode
 from skills.tree import add_tree
 from skills.uwu import add_uwu
 
-__version__ = "0.22"
+__version__ = "0.23"
 
 logger = logging.getLogger(__name__)
 
