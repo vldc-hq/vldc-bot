3ebb51034bdc4ed5244b4538f69d9178cb0a3bc1
egregors <root@egregors.com>
2020-09-17 21:37:48 +1000

    feat: simple top cmd for words

--- bot/skills/words.py
+++ bot/skills/words.py
@@ -2,11 +2,15 @@ import logging
 from datetime import datetime
 from typing import List
 
+import pymongo
+import telegram
 from pymongo.collection import Collection
 from telegram import Update
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, run_async
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, run_async, CommandHandler
 
 from db.mongo import get_db
+from filters import admin_filter
+from mode import cleanup_update_context
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +29,9 @@ class DB:
         for word in words:
             self.add_word(word)
 
+    def find_all(self):
+        return self._coll.find({}).sort("count", pymongo.DESCENDING)
+
 
 _db = DB(db_name='words')
 
@@ -32,6 +39,7 @@ _db = DB(db_name='words')
 def add_words(upd: Updater, handlers_group: int):
     logger.info("register words handlers")
     dp = upd.dispatcher
+    dp.add_handler(CommandHandler("top", show_top, filters=admin_filter), handlers_group)
     dp.add_handler(MessageHandler(Filters.text, extract_words), handlers_group)
 
 
@@ -45,4 +53,15 @@ def _get_words(t: str) -> List[str]:
 
 
 def _normalize(words: List[str]) -> List[str]:
-    return [w.lower() for w in words]
+    return [w.lower() for w in words if w[0] != '/']
+
+
+@run_async
+@cleanup_update_context(seconds=600, remove_cmd=True, remove_reply=True)
+def show_top(update: Update, context: CallbackContext):
+    _top_limit = 10
+    # TODO: make it pretty
+    top = "\n".join([f"{w['word']}: {w['count']}" for w in list(_db.find_all())[:_top_limit]])
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=f"```\n{top}\n```", disable_notification=True,
+                             parse_mode=telegram.ParseMode.MARKDOWN)
