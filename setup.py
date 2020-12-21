e60fff06e1452fffb18bb24dad6e2770de02a375
egregors <root@egregors.com>
2020-12-21 23:54:19 +0300

    deps: update

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ readme = ''
 setup(
     long_description=readme,
     name='vldc-bot',
-    version='0.4.11',
+    version='0.5.1',
     description='VLDC nyan bot ^_^',
-    python_requires='==3.*,>=3.8.0',
+    python_requires='==3.*,>=3.9.0',
     author='@egregors',
     maintainer='@cpro29a',
     packages=['bot', 'bot.db', 'bot.skills', 'bot.tests', 'bot.utils'],
@@ -24,8 +24,9 @@ setup(
     package_data={},
     install_requires=[
         'google-cloud-automl==0.10.0', 'google-cloud-speech==2.0.0',
-        'google-cloud-translate==2.0.0', 'pillow==8.*,>=8.0.1',
-        'pymongo==3.11.0', 'python-telegram-bot==13.0', 'sentry-sdk==0.19'
+        'google-cloud-translate==2.0.0', 'pillow==8.*,>=8.0.0',
+        'pymongo==3.*,>=3.11.0', 'python-telegram-bot==13.0',
+        'sentry-sdk==0.*,>=0.19.0'
     ],
     extras_require={
         "dev": [
