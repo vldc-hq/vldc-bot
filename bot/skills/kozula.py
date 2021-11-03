13f49f062e638260c394850c9b1c12653c529a13
egregors <root@egregors.com>
2021-11-03 12:42:50 +0200

    fix: linter satisfaction

--- bot/skills/kozula.py
+++ bot/skills/kozula.py
@@ -1,6 +1,6 @@
 import logging
-import requests
 import xml.etree.ElementTree as ET
+import requests
 from mode import cleanup_queue_update
 
 from telegram import Update
@@ -9,7 +9,8 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 logger = logging.getLogger(__name__)
 
 KOZULA_RATE = 600000
-CBR_URL = 'https://cbr.ru/scripts/XML_daily.asp'
+CBR_URL = "https://cbr.ru/scripts/XML_daily.asp"
+
 
 def add_kozula(upd: Updater, handlers_group: int):
     logger.info("registering tree handlers")
@@ -21,13 +22,13 @@ def kozula(update: Update, context: CallbackContext):
     request = requests.get(CBR_URL)
     root = ET.fromstring(request.content)
     for child in root:
-        if child.attrib["ID"] == 'R01235':
-            usd_rate = float(child.find('Value').text.replace(',', '.'))
+        if child.attrib["ID"] == "R01235":
+            usd_rate = float(child.find("Value").text.replace(",", "."))
 
     logger.info("USD rate: %s", usd_rate)
     kozula_usd = round(KOZULA_RATE / usd_rate, 2)
     kozula_usd_formated = "${:,.2f}".format(kozula_usd)
-    kozula_rub_formated = "{:,} ₽".format(KOZULA_RATE).replace(',', ' ')
+    kozula_rub_formated = "{:,} ₽".format(KOZULA_RATE).replace(",", " ")
 
     text = (
         f"Текущий курс Козули в RUB: {kozula_rub_formated}\n"
