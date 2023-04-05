2e536708d593db8c1d9beeaaa9045d7d561beb4a
Aleksandr Sutulov <sutulovalexander@gmail.com>
2023-04-05 20:55:49 +0200

    dev: add video_notes recognition to nastya mode

--- bot/utils/voice_recognition.py
+++ bot/utils/voice_recognition.py
@@ -1,10 +1,11 @@
 import json
+from typing import Tuple, Union, Optional
 import logging
 import os
 import subprocess
 from tempfile import NamedTemporaryFile
-
 import requests
+
 from google.cloud import speech
 
 TOKEN = os.environ["TOKEN"]
@@ -15,6 +16,12 @@ ENCODING_TYPE = "LINEAR16"
 CHANNEL_COUNT = 1
 ENABLE_AUTOMATIC_PUNCTUATION = True
 SPEECH_MODEL = "default"
+
+OGA = ".oga"
+MP4 = ".mp4"
+
+VIDEO = "video"
+AUDIO = "audio"
 
 logger = logging.getLogger(__name__)
 
@@ -39,32 +46,32 @@ except:  # noqa
     speech_client = Dummy()
 
 
-def _get_audio_file_data(file_id):
-    """Gets audio file."""
+def _get_tg_resource(file_id: str) -> Tuple[bytes, Union[AUDIO, str]]:
     url = f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}"
     response = requests.get(url)
     json_response = response.json()
-    logger.info(
-        "Voice data has been obtained: \n%s", json.dumps(json_response, indent=4)
-    )
-    return json_response["result"]
-
-
-def _get_binary_content(file):
-    file_name = file["file_path"]
-    url = f"https://api.telegram.org/file/bot{TOKEN}/{file_name}"
+    logger.info("Data has been obtained: \n%s", json.dumps(json_response, indent=4))
+    file_path = json_response["result"]["file_path"]
+    url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
     response = requests.get(url, stream=True)
-    suffix = ".oga"
 
-    with NamedTemporaryFile(suffix=suffix) as tmp_voice:
-        tmp_voice.write(response.content)
+    return (response.content, AUDIO) if OGA in file_path else (response.content, VIDEO)
+
+
+def _get_converted_audio_content(buffer: bytes) -> Optional[bytes]:
+    with NamedTemporaryFile(suffix=OGA) as tmp_voice:
+        tmp_voice.write(buffer)
 
         tmp_voice_filename_ogg = tmp_voice.name
-        tmp_voice_filename_wav = tmp_voice_filename_ogg[: -len(suffix)] + ".wav"
+        tmp_voice_filename_wav = tmp_voice_filename_ogg[: -len(OGA)] + ".wav"
 
-        subprocess.call(
-            ["ffmpeg", "-i", tmp_voice_filename_ogg, tmp_voice_filename_wav]
-        )
+        try:
+            subprocess.call(
+                ["ffmpeg", "-i", tmp_voice_filename_ogg, tmp_voice_filename_wav]
+            )
+        except subprocess.CalledProcessError as err:
+            logger.error("error with ffmpeg while processing %s: %s", OGA, err)
+            return None
 
         with open(tmp_voice_filename_wav, "rb") as converted_wav:
             converted_wav_bin = converted_wav.read()
@@ -86,34 +93,73 @@ def _send_binary_to_google_speech(content):
     }
     audio = {"content": content}
     # noinspection PyTypeChecker
-    speech_response = speech_client.recognize(config=config, audio=audio)
-    return speech_response
+    return speech_client.recognize(config=config, audio=audio)
 
 
-def _check_google_speech_response(response):
+def _check_google_speech_response(response) -> str:
     result = response.results[-1]
     logger.info("Google speech response results: %s", result)
+
     any_alternatives = result.alternatives or []
-    if len(any_alternatives) > 0:
-        transcription = any_alternatives[0].transcript
-        return transcription
-    return None
+    if len(any_alternatives) == 0:
+        return None
+
+    transcription = any_alternatives[0].transcript
+    return transcription
 
 
-def get_text_from_speech(file_id):
-    """Gets text from voice."""
+def _get_converted_video_content(buffer: bytes) -> Optional[bytes]:
+    with NamedTemporaryFile(suffix=MP4) as tmp_video:
+        tmp_video.write(buffer)
+
+        tmp_video_filename_mp4 = tmp_video.name
+        tmp_audio_filename_wav = tmp_video_filename_mp4[: -len(MP4)] + ".wav"
+
+        try:
+            subprocess.call(
+                [
+                    "ffmpeg",
+                    "-i",
+                    tmp_video_filename_mp4,
+                    "-acodec",
+                    "libvorbis",
+                    tmp_audio_filename_wav,
+                ]
+            )
+        except subprocess.CalledProcessError as err:
+            logger.error("error with ffmpeg while processing %s: %s", MP4, err)
+            return None
+
+        with open(tmp_audio_filename_wav, "rb") as converted_wav:
+            converted_wav_bin = converted_wav.read()
+
+        os.remove(tmp_audio_filename_wav)
+
+    return converted_wav_bin
+
+
+def get_recognized_text(file_id: str):
     try:
-        file = _get_audio_file_data(file_id)
-        content = _get_binary_content(file)
-        response = _send_binary_to_google_speech(content)
+        bin_data, file_type = _get_tg_resource(file_id)
+
+        converted_content = (
+            _get_converted_audio_content(bin_data)
+            if file_type == AUDIO
+            else _get_converted_video_content(bin_data)
+        )
+        if converted_content is None:
+            return converted_content
+
+        response = _send_binary_to_google_speech(converted_content)
         result = _check_google_speech_response(response)
         logger.info("Result of voice recognition: %s", result)
         return result
     except (AttributeError, ValueError, RuntimeError) as ex:
         logger.error(
-            "Error during voice recognition %s", {"exception": ex, "file_id": file_id}
+            "Error during the voice recognition %s",
+            {"exception": ex, "file_id": file_id},
         )
         return None
 
 
-__all__ = ["get_text_from_speech"]
+__all__ = ["get_recognized_text"]
